6cf430d635d5acb796de13cf0f67bad93ae3d97c
xufeng <xufeng@bitnei.cn>
2022-01-06 10:20:06 +0800

    first commit

---
+++ src/main/java/com/day/dayup/DayupApplication.java
@@ -0,0 +1,13 @@
+package com.day.dayup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayupApplication.class, args);
+    }
+
+}
